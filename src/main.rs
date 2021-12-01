758605335c5464776ddd0f18f63454424bbcf7a5
Kindritskiy Max <kindritskiy.m@gmail.com>
2021-12-01 18:34:28 +0200

    solve first puzzle part 2

--- src/main.rs
+++ src/main.rs
@@ -31,12 +31,32 @@ fn count_depht_increase(input: &Vec<usize>) -> usize {
     increase_count
 }
 
+fn get_compated_data(input: &Vec<usize>) -> Vec<usize> {
+    let mut compacted_data = Vec::new();
+    let mut window = vec![];
+    for val in input.iter() {
+        if window.len() < 3 {
+            window.push(val.clone());
+        } else {
+            compacted_data.push(window.iter().sum());
+            window.pop();
+            window.insert(0, val.clone());
+        }
+    }
+
+    compacted_data.push(window.iter().sum());
+
+    compacted_data
+}
+
 fn main() {
 
     let input = read_input("input.txt").unwrap();
 
-    let count = count_depht_increase(&input);
-    println!("Increases count: {}", count);
+    let compacted_data = get_compated_data(&input);
+        
+    let count = count_depht_increase(&compacted_data);
+    println!("Increases count with sliding window: {}", count);
 }
 
 
@@ -47,6 +67,15 @@ mod tests {
     fn test_count_depth_increases() {
         let input = vec![1, 2, 1, 4, 5, 3];
         let count = count_depht_increase(&input);
+
+        assert_eq!(count, 3)
+    }
+    #[test]
+    fn test_count_depth_increase_sliding_window() {
+        let input = vec![1, 2, 1, 2, 3, 1, 3];
+        let compacted_data = get_compated_data(&input);
+        assert_eq!(compacted_data, vec![4, 5, 6, 6, 7]);
+        let count = count_depht_increase(&compacted_data);
 
         assert_eq!(count, 3)
     }
